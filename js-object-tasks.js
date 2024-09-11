8a8625c71bf48b3ba26b8b2148611a523c63b89f
annika02 <at621504@gmail.com>
2024-09-12 00:15:50 +0600

    first commit

---
+++ js-object-tasks.js
@@ -0,0 +1,67 @@
+// Task-1
+// Access the golden rod color value in output.
+
+const colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff",
+    "golden rod": '#daa520'
+};
+
+console.log(colors["golden rod"]);
+// Task-2
+// For this object below add a property named passenger capacity with value 5
+
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+car.passenger = 5;
+console.log(car)
+
+// Task-3
+// Display the physics marks as output.
+
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+console.log(student["physics"]["marks"]);
+// Task-4
+// Count the number of properties.
+
+// Input:
+
+let student1 = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+console.log(Object.keys(student1).length);
+// Task-5 (Hard)
+// Loop through an object and print the key-value pairs with their types
+
+// Input:
+
+let myObject = {
+
+name: 'John Doe',
+age: 25,
+city: 'Example City',
+isStudent: true
+};
+
+
+
+
+// key: name | type:  string
+// key: age | type:  number
+// key: city | type:  string
+// key: isStudent | type:  boolean
